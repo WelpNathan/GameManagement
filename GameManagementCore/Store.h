da2cab75b9d6846a5bf2569bb78390b340d583a0
Nathan Hall <welpnathan@gmail.com>
2021-03-25 13:54:15 +0000

    Add ListT support to store

--- GameManagementCore/Store.h
+++ GameManagementCore/Store.h
@@ -1,12 +1,14 @@
 #pragma once
 
 #include "Game.h"
+#include "ListT.h"
 
 class store
 {
 public:
 	store();
 	~store();
-	game* games[9] = {}; // TODO: should be a dynamic collection
+	//game* games[9] = {}; // TODO: should be a dynamic collection
+	List<game*> games;
 private:
 };
