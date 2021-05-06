16f3bd0685e184fd461752545af0fe83b496b803
Claire Storey <clairestorey30614@gmail.com>
2021-05-06 21:52:02 +0100

    attempt to fix error

--- GameManagementCore/loginMenu.cpp
+++ GameManagementCore/loginMenu.cpp
@@ -22,34 +22,38 @@ void login_menu::output_options()
 
 bool login_menu::handle_choice(const char choice)
 {
-	const int index = utils::char_to_int(choice) - 1;
-	account* this_account = app_->get_current_account();
-	int count = 0;
-
-	while (count < 3)
+	switch (choice)
 	{
-		if (index >= 0)
-		{
-			const int size = this_account->users.length();
-
-			if (index >= 0 && index < size)
-			{
-				const std::string this_username = this_account->users[index]->get_username();
-				const std::string stored_password = this_account->users[index]->get_password();
-				
-				if (check_password(this_username, stored_password) == true)
-				{
-					app_->login_user(index, this_username, stored_password);
-					main_menu this_menu("MAIN MENU", app_);
-				}
-				else
-				{
-					count++;
-					std::cout << "\n\n" << "  Password incorrect - Please Try again..." << "\n\n";
-				}
-			}
-		}
+	case '1':
+	{
+		const int i = 0;
+		const int j = app_->get_current_account()->get_id();
+		const std::string this_username = app_->get_current_account()->users[i]->get_username();
+		const std::string stored_password = app_->get_current_account()->users[i]->get_password();
+		check_password(i, j, this_username, stored_password);
 	}
+	break;
+	case '2':
+	{
+		const int i = 1;
+		const int j = app_->get_current_account()->get_id();
+		const std::string this_username = app_->get_current_account()->users[i]->get_username();
+		const std::string stored_password = app_->get_current_account()->users[i]->get_password();
+		check_password(i, j, this_username, stored_password);
+	}
+	break;
+	case '3':
+	{
+		const int i = 2;
+		const int j = app_->get_current_account()->get_id();
+		const std::string this_username = app_->get_current_account()->users[i]->get_username();
+		const std::string stored_password = app_->get_current_account()->users[i]->get_password();
+		check_password(i, j, this_username, stored_password);
+	}
+	break;
+	default:;
+	}
+
 	return false;
 }
 
